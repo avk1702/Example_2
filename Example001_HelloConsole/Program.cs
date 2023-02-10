40c6d82f127cf0e604738a7b9074c3c8189b5057
avk1702 <avk1702@bk.ru>
2023-02-10 15:46:20 +0500

    change file

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class MyProgram
+{
+    private static Random random = new Random();
+    
+    public static void Main(string[] args)
+    {
+        int size;
+        
+        size = 10;
+        int[] col = new int[size];
+        int index;
+        
+        index = 0;
+        while (index < size)
+        {
+            col[index] = random.Next(10);
+            Console.Write(col[index].ToString() + " ");
+            index = index + 1;
+        }
+        int i;
+        int j;
+        
+        i = 0;
+        while (i < size)
+        {
+            j = 0;
+            while (j < size - 1)
+            {
+                if (col[j] > col[j + 1])
+                {
+                    int temp;
+                    
+                    temp = col[j];
+                    col[j] = col[j + 1];
+                    col[j + 1] = temp;
+                }
+                j = j + 1;
+            }
+            i = i + 1;
+        }
+        index = 0;
+        Console.WriteLine("");
+        while (index < size)
+        {
+            Console.Write(col[index].ToString() + " ");
+            index = index + 1;
+        }
+    }
+}
