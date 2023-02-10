c462a7e695310bf4731932a3b86bd59fe308a3ed
avk1702 <avk1702@bk.ru>
2023-02-10 12:10:32 +0500

    Random ().Next(мin, маx)

--- Еxample003_Sum/Program.cs
+++ Еxample003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 37;
-int numberВ = 59;
-int result = numberA + numberВ;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
